301168ae19865d5e2cf5d1e30cd39cc919ece8c9
Jonathan Yeo <jonathanyji98_28@hotmail.com>
2024-06-17 11:21:04 +1000

    delete works

--- server/controllers/mediaController.js
+++ server/controllers/mediaController.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 const fs = require('fs');
-const { PutObjectCommand } = require('@aws-sdk/client-s3');
+const { PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
 const Media = require('../models/mediaModel');
 const { S3Client } = require('@aws-sdk/client-s3');
 
@@ -20,83 +20,83 @@ const storage = multer.diskStorage({
 const s3Client = new S3Client({
     region: process.env.REGION,
     credentials: {
-      accessKeyId: process.env.ASSESS_KEY_ID,
-      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        accessKeyId: process.env.ASSESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
     },
-  });
+});
 
 const upload = multer({ storage: storage });
 
 exports.createMedia = [
-    upload.single('file'), 
+    upload.single('file'),
     body('name').isString().isLength({ min: 1 }).trim().escape(),
 
     async (req, res) => {
-        try{
+        try {
             // const token = req.header('Authorization').replace('Bearer ', '');
             // const decoded = jwt.verify(token, process.env.JWT_SECRET);
             // const userId = decoded.userId;
 
-        // if (!userId) {
-        //     return res.status(401).json({ error: 'Unauthorized' });
-        // }
-        
-
-        const { filename, mimetype, size } = req.file;
+            // if (!userId) {
+            //     return res.status(401).json({ error: 'Unauthorized' });
+            // }
 
 
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        
-        const tmpFile = await fs.createReadStream('tmp/' + filename);
+            const { filename, mimetype, size } = req.file;
 
-        const tempUserId = 1;
-        try {
-          const command = new PutObjectCommand({
-            Bucket: process.env.S3_BUCKET_NAME,
-            Key: tempUserId + "/" + filename,
-            Body: tmpFile,
-            ContentType: mimetype,
-          });
-        
-            const response = await s3Client.send(command);
-            console.log(response);
-          } catch (err) {
-            console.error("S3 upload error: ", err);
-          }
 
-        const media = {
-            name: req.body.name,
-            fileType: mimetype,
-            uploadedByID: tempUserId,
-            filePath: tempUserId + "/" + filename,
-            fileSize: size,
-        };
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
-        try {
-            const id = await Media.create(media);
-            const filePath = 'tmp/' + filename;
+            const tmpFile = await fs.createReadStream('tmp/' + filename);
 
-            fs.unlink(filePath, (err) => {
-                if (err) {
-                  console.error(err);
-                } else {
-                  console.log('Clean up tmp folder.');
-                }
-              });
-            res.status(201).json({ id, ...media });
+            const tempUserId = 1;
+            try {
+                const command = new PutObjectCommand({
+                    Bucket: process.env.S3_BUCKET_NAME,
+                    Key: tempUserId + "/" + filename,
+                    Body: tmpFile,
+                    ContentType: mimetype,
+                });
+
+                const response = await s3Client.send(command);
+                console.log(response);
+            } catch (err) {
+                console.error("S3 upload error: ", err);
+            }
+
+            const media = {
+                name: req.body.name,
+                fileType: mimetype,
+                uploadedByID: tempUserId,
+                filePath: tempUserId + "/" + filename,
+                fileSize: size,
+            };
+
+            try {
+                const id = await Media.create(media);
+                const filePath = 'tmp/' + filename;
+
+                fs.unlink(filePath, (err) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log('Clean up tmp folder.');
+                    }
+                });
+                res.status(201).json({ id, ...media });
+            } catch (err) {
+                console.log("SERVER ERROR: ", err);
+                res.status(500).json({ error: 'Database error' });
+            }
+
         } catch (err) {
-            console.log("SERVER ERROR: ", err);
-            res.status(500).json({ error: 'Database error' });
-        }
-
-        } catch (err){
             console.log("SERVER ERROR: ", err);
             res.status(500).json({ error: 'Internal Server Error' });
         }
-        
+
     }
 ];
 
@@ -106,7 +106,7 @@ exports.getAllMedia = async (req, res) => {
         const media = await Media.findAll(userId);
         res.json(media);
     } catch (err) {
-        res.status(500).json({ error: 'Database error' });
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 };
 
@@ -125,7 +125,7 @@ exports.getMediaById = async (req, res) => {
         }
         res.json(media);
     } catch (err) {
-        res.status(500).json({ error: 'Database error' });
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 };
 
@@ -136,25 +136,25 @@ exports.updateMedia = [
 
     async (req, res) => {
 
-        try{
+        try {
             const token = req.header('Authorization').replace('Bearer ', '');
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             const userId = decoded.userId;
-    
+
             if (!userId) {
                 return res.status(401).json({ error: 'Unauthorized' });
             }
-    
+
             const id = parseInt(req.params.id, 10);
             if (isNaN(id)) {
                 return res.status(400).json({ error: 'Invalid ID' });
             }
-    
+
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 return res.status(400).json({ errors: errors.array() });
             }
-    
+
             const media = {
                 name: req.body.name,
                 fileType: req.body.fileType,
@@ -162,42 +162,58 @@ exports.updateMedia = [
                 filePath: userId + "/file_location",
                 fileSize: req.body.fileSize,
             };
-    
+
             try {
                 const existingMedia = await Media.findById(id);
                 if (!existingMedia) {
                     return res.status(404).json({ error: 'Media not found' });
                 }
-    
+
                 await Media.update(id, media);
                 res.json({ id, ...media });
             } catch (err) {
                 res.status(500).json({ error: 'Database error' });
             }
 
-        } catch (err){
+        } catch (err) {
             res.status(500).json({ error: 'Internal Server Error' });
         }
 
-       
+
     }
 ];
 
 exports.deleteMedia = async (req, res) => {
     const id = parseInt(req.params.id, 10);
+    const tempUserId = 1;
+    console.log("DELETE api called")
     if (isNaN(id)) {
         return res.status(400).json({ error: 'Invalid ID' });
     }
 
     try {
-        const existingMedia = await Media.findById(id);
+        const existingMedia = await Media.findById(id, tempUserId);
         if (!existingMedia) {
             return res.status(404).json({ error: 'Media not found' });
+        }
+
+        //Delete from s3
+        try {
+            const fileLocation = existingMedia.filePath
+            const command = new DeleteObjectCommand({
+                Bucket: process.env.S3_BUCKET_NAME,
+                Key: fileLocation,
+            });
+
+            const response = await s3Client.send(command);
+            console.log(response);
+        } catch (err) {
+            console.error("S3 delete file error: ", err);
         }
 
         await Media.delete(id);
         res.status(204).send();
     } catch (err) {
-        res.status(500).json({ error: 'Database error' });
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 };
